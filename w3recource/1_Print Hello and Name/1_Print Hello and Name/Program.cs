744bd3fbce886e33e41a4037425246709c9cc26b
elvinyekayev <elvinyekay@gmail.com>
2026-02-01 02:38:41 +0400

    w3resource first algorithm

---
+++ w3recource/1_Print Hello and Name/1_Print Hello and Name/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+ * Write a C# Sharp program to print Hello and your name in a separate line.
+ */
+
+string name = "Elvin";
+
+Console.WriteLine("Hello, ");
+Console.WriteLine(name);
