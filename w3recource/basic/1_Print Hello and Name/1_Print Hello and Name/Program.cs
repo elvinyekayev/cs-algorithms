1ea9ae102e658edcfebdb0da2f7c75a2347e959d
elvinyekayev <elvinyekay@gmail.com>
2026-02-14 16:33:53 +0400

    print hello and name

--- w3recource/basic/1_Print Hello and Name/1_Print Hello and Name/Program.cs
+++ w3recource/basic/1_Print Hello and Name/1_Print Hello and Name/Program.cs
@@ -1,9 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
 /*
- * Write a C# Sharp program to print Hello and your name in a separate line.
+ * 1.Write a C# Sharp program to print Hello and your name in a separate line.
+ * 2.Write a C# program to print a greeting message using string interpolation and environment variables to include the current username.
+   3.Write a C# program that prints “Hello” and the full name read dynamically from user input, and then display the name in reverse order on a new line.
+   4.Write a C# program that displays the greeting using a randomly generated title (Mr., Ms., Dr.) followed by the user's name entered via the console.
+   5.Write a C# program to print "Hello" and a name from a list of names stored in an array, selected by index input from the user.
  */
 
+// 1. Write a C# Sharp program to print Hello and your name in a separate line.
 string name = "Elvin";
 
 Console.WriteLine("Hello, ");
 Console.WriteLine(name);
+
+
+//2. Write a C# program to print a greeting message using string interpolation and environment variables to include the current username.
+// string message = $"Hello, my name is {firstName} {lastName} and I am {age} years old.";
+
+string username = Environment.UserName;
+string greetingMessage = $"Hello, {username}! Welcome to C# programming.";
+Console.WriteLine(greetingMessage);
+
+//3 Write a C# program that prints “Hello” and the full name read dynamically from user input, and then display the name in reverse order on a new line.
+// Console.WriteLine("Enter your full name");
+// string fullName = Console.ReadLine();
+// string reversedFullName = new string(fullName.Reverse().ToArray());
+// Console.WriteLine($"Hello  + {fullName}!");
+// Console.WriteLine(reversedFullName);
+
+//4. Write a C# program that displays the greeting using a randomly generated title (Mr., Ms., Dr.) followed by the user's name entered via the console.
+
+// string[] titles = ["Mr.", "Ms.","Dr."];
+// Random random = new Random();
+// int index = random.Next(titles.Length);
+// Console.WriteLine(titles[index]);
+// Console.WriteLine("Enter a name");
+// string userName = Console.ReadLine();
+// Console.WriteLine($"Hello {titles[index]} {userName}!");
+
+//5. Write a C# program to print "Hello" and a name from a list of names stored in an array, selected by index input from the user.
+string[] names = ["Elvin", "John", "Alice", "Bob", "Eve"];
+Console.WriteLine("Enter index of the name you want to select (1-5):");
+var index = int.Parse(Console.ReadLine()) - 1;
+
+Console.WriteLine($"Hello {names[index]}!");
---
+++ w3recource/basic/2_Sum of Two Numbers  /2_Sum of Two Numbers  /Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * 1.Write a C# Sharp program to print the sum of two numbers.
+ * 2.Write a C# program that takes two floating-point numbers, rounds them, and then calculates their sum.
+   3.Write a C# program that reads two hexadecimal numbers from input and prints their sum in decimal form.
+   4.Write a C# program to add two user-entered values but swap them using XOR logic before adding.
+   5.Write a C# program that calculates the sum of two randomly generated integers between 100 and 999 and checks if the result is even.
+ */
+
+
+//1.Write a C# Sharp program to print the sum of two numbers.
+// Console.WriteLine("Enter first number: ");
+// int a = int.Parse(Console.ReadLine());
+// Console.WriteLine("Enter second number: ");
+// int b = int.Parse(Console.ReadLine());
+
+// Console.WriteLine(a + b);
+
+//2.Write a C# program that takes two floating-point numbers, rounds them, and then calculates their sum.
+// Console.WriteLine("Enter first number: ");
+// double a = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Enter second number: ");
+// double b = Math.Round(Convert.ToDouble(Console.ReadLine()));
+//
+// double roundedA =Math.Round(a);
+// double roundedB = Math.Round(b);
+// var result = roundedA + roundedB;
+//
+// Console.WriteLine("The result is: " + result);
+
+//3.Write a C# program that reads two hexadecimal numbers from input and prints their sum in decimal form.
+
+// Console.WriteLine("Enter first hex number:");
+// string firstHex = Console.ReadLine();
+// Console.WriteLine("Enter second hex number:");
+// string secondHex = Console.ReadLine();
+//
+// int firstNum = Convert.ToInt32(firstHex, 16);
+// int secondNum = Convert.ToInt32(secondHex, 16);
+//
+// int sum = firstNum + secondNum;
+// Console.WriteLine("Converted first number is: " + firstNum);
+// Console.WriteLine("Converted second number is: " + secondNum);
+// Console.WriteLine("The sum is: " + firstHex +  " and " + secondHex +  " is: " + sum);
+
+//   4.Write a C# program to add two user-entered values but swap them using XOR logic before adding.
+
+/*
+ * Klassik swap (əlavə dəyişənlə):
+   temp = a
+   a = b
+   b = temp
+   XOR swap (əlavə dəyişən OLMADAN):
+   a = a ^ b
+   b = a ^ b
+   a = a ^ b
+ */
+
+// Console.WriteLine("Enter the first number: ");
+// int firstNumber = int.Parse(Console.ReadLine());
+// Console.WriteLine("Enter the second number: ");
+// int secondNumber = int.Parse(Console.ReadLine());
+//
+// firstNumber = firstNumber ^ secondNumber;
+// secondNumber = firstNumber ^ secondNumber;
+// firstNumber = firstNumber ^ secondNumber;
+//
+// Console.WriteLine("firstNumber:  " + firstNumber);
+// Console.WriteLine("secondNumber : " + secondNumber);
+//
+// Console.WriteLine("Sum of two numbers: " + (firstNumber + secondNumber));
+
+// 5.Write a C# program that calculates the sum of two randomly generated integers between 100 and 999 and checks if the result is even.
+
+Random random = new Random();
+int a = random.Next(100, 999);
+int b = random.Next(100, 999);
+
+Console.WriteLine("a: " + a);
+Console.WriteLine("b: " + b);
+Console.WriteLine("Sum: " + (a + b));
+
+Console.WriteLine((a + b) % 2 == 0 ? "even" : "odd");
