920fb13c9df45f1e3ff577e38b965fc7dd9bc4e1
elvinyekayev <elvinyekay@gmail.com>
2026-02-18 23:59:17 +0400

    Specified Operations Results

---
+++ w3recource/basic/4_Specified_Operations_Results/4_Specified_Operations_Results/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * 1.Write a C# Sharp program to print the results of the specified operations.
+   Test data:
+   
+   -1 + 4 * 6
+   ( 35+ 5 ) % 7
+   14 + -4 * 6 / 11
+   2 + 15 / 6 * 1 - 7 % 2
+   Expected Output:
+   23
+   5
+   12
+   3
+ */
+
+Console.WriteLine("-1 + 4 * 6 = " + (-1 + 4 * 6));
+Console.WriteLine("( 35+ 5 ) % 7 = " + ((35 + 5) % 7));
+Console.WriteLine("14 + -4 * 6 / 11 = " + (14 + -4 * 6 / 11));
+Console.WriteLine("2 + 15 / 6 * 1 - 7 % 2 = " + (2 + 15 / 6 * 1 - 7 % 2));
+
