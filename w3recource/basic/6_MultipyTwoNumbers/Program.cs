1f6e4e109844881ae20a0bd2668effb04e1abbf7
ElvinYekayev <elvinyekay@gmail.com>
2026-02-22 14:23:14 +0400

    Arithmetic operation

---
+++ w3recource/basic/6_MultipyTwoNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿/**
+ * 
+ * Write a C# Sharp program to print the output of the multiplication of three numbers entered by the user.
+Test Data:
+Input the first number to multiply: 2
+Input the second number to multiply: 3
+Input the third number to multiply: 6
+Expected Output:
+2 x 3 x 6 = 36
+ * 
+ */
+
+
+Console.WriteLine("Input the first number to multiply");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Input the second number to multiply:");
+int secondNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Input the third number to multiply: ");
+int thirdNumber = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"The result of {firstNumber} * {secondNumber} * {thirdNumber} = {firstNumber * secondNumber * thirdNumber}");
---
+++ w3recource/basic/7_ArithmeticOperator/Program.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * 
+ * Write a C# Sharp program to print on screen the output of adding, subtracting, multiplying and dividing two numbers 
+ * entered by the user.
+Test Data:
+Input the first number: 25
+Input the second number: 4
+Expected Output:
+25 + 4 = 29
+25 - 4 = 21
+25 x 4 = 100
+25 / 4 = 6
+25 mod 4 = 1
+ * 
+ * */
+
+
+Console.WriteLine("Input the first number:");
+int firstNum = int.Parse(Console.ReadLine());
+Console.WriteLine("Input the second number:");
+int secondNum = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{firstNum} + {secondNum} = {firstNum + secondNum}");
+Console.WriteLine($"{firstNum} - {secondNum} = {firstNum - secondNum}");
+Console.WriteLine($"{firstNum} * {secondNum} = {firstNum * secondNum}");
+Console.WriteLine($"{firstNum} / {secondNum} = {firstNum / secondNum}");
+Console.WriteLine($"{firstNum} mod {secondNum} = {firstNum % secondNum}");
+
