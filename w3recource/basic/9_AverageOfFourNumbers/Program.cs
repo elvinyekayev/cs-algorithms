16ba0439fa5278f3cd08c43e29b7e543bb76ba7b
ElvinYekayev <elvinyekay@gmail.com>
2026-02-22 15:04:35 +0400

    algo 10

---
+++ w3recource/basic/9_AverageOfFourNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Write a C# Sharp program that takes four numbers as input to calculate and print the average.
+Test Data:
+Enter the First number: 10
+Enter the Second number: 15
+Enter the third number: 20
+Enter the four number: 30
+
+Expected Output:
+The average of 10 , 15 , 20 , 30 is: 18
+ */
+Console.WriteLine("Enter the First number:");
+int first = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second number: ");
+int second = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Enter the third number: ");
+int third = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the fpurth number: ");
+int fourth = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"The average of {first}, {second}, {third}, {fourth} is :  {(first + second + third + fourth)/4}");
---
+++ w3recource/basic/WithThreeNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * 
+ * Write a C# Sharp program that takes three numbers (x,y,z) as input and outputs (x+y).z and x.y + y.z.
+Test Data:
+Enter first number - 5
+Enter second number - 6
+Enter third number - 7
+
+Expected Output:
+Result of specified numbers 5, 6 and 7, (x+y).z is 77 and x.y + y.z is 72
+ * 
+ */
+
+Console.WriteLine("Enter the first number");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number: ");
+int second = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the third number: ");
+int third = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"({first} + {second}) * {third} = {(first + second) * third}");
+Console.WriteLine($"{first} * {second} +  {second} + {third} = {(first * second) + (second * third)}");
